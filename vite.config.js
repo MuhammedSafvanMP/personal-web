9b991dc1ddee1c1d0ec3957472c6632803789a5e
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-05-09 00:04:29 +0530

    feat: prot number change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(), react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 3290,
+  },
+  // preview: {
+  //   host: '0.0.0.0',
+  //   port: 7867,
+  //   allowedHosts: ['e-learning-5x2n.onrender.com'], 
+  // },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
